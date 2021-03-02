66c8fda113ba51c8e26114e5ebc46f9519c5b4c4
Your Name <you@example.com>
2021-03-02 10:39:46 -0600

    first-commit dom 1

--- js/index.js
+++ js/index.js
@@ -37,6 +37,66 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+ let logo = document.getElementById("logo-img");
+ logo.setAttribute('src',siteContent["nav"]["img-src"])
+ let topimage=document.querySelector("#cta-img");
+ topimage.setAttribute("src",siteContent["cta"]["img-src"])
+
+ let navigation = document.querySelectorAll("nav a");
+
+ navigation.forEach((item,index)=> {
+   
+item.textContent=siteContent.nav[`nav-item-${index + 1}`];
+ })
+
+ const h1 = document.querySelector('.cta h1');
+h1.textContent = siteContent.cta.h1;
+
+const button1 = document.querySelector('.cta button');
+button1.textContent = siteContent.cta.button;
+
+document.querySelector('#middle-img').src = siteContent["main-content"]["middle-img-src"];
+
+const mainContentHeader = document.querySelectorAll('.main-content h4');
+const mainContentPara = document.querySelectorAll('.main-content p');
+
+
+mainContentHeader[0].textContent=siteContent['main-content']["features-h4"]
+mainContentHeader[1].textContent = siteContent["main-content"]["about-h4"];
+mainContentHeader[2].textContent = siteContent["main-content"]["services-h4"];
+mainContentHeader[3].textContent = siteContent["main-content"]["product-h4"];
+mainContentHeader[4].textContent = siteContent["main-content"]["vision-h4"];
+
+mainContentPara[0].textContent = siteContent["main-content"]["features-content"];
+mainContentPara[1].textContent = siteContent["main-content"]["about-content"];
+mainContentPara[2].textContent = siteContent["main-content"]["services-content"];
+mainContentPara[3].textContent = siteContent["main-content"]["product-content"];
+mainContentPara[4].textContent = siteContent["main-content"]["vision-content"];
+
+const contactHeader = document.querySelector('.contact h4');
+const contactPara = document.querySelectorAll('.contact p');
+
+contactHeader.textContent = siteContent.contact["contact-h4"];
+contactPara[0].textContent = siteContent.contact["address"];
+contactPara[1].textContent = siteContent.contact["phone"];
+contactPara[2].textContent = siteContent.contact.email;
+
+
+const newNavigation1 = document.createElement('a');
+const newnavigation2 = document.createElement('a');
+newNavigation1.href = '#';
+newnavigation2.href = "#";
+newNavigation1.textContent = "I'm not";
+newnavigation2.textContent = "Original";
+
+
+
+const anotherNav = document.querySelector('header nav');
+anotherNav.prepend(newNavigation1);
+anotherNav.appendChild(newnavigation2);
+
+newNavigation1.style.color = "blue";
+newnavigation2.style.color = "green";
+
+
+
