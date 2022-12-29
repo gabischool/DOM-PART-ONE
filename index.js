cf870d9bbce0b763ad297aaa967250cce675c596
hanaa filsan <116567403+Hanaafilsan@users.noreply.github.com>
2022-12-29 18:22:07 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+const siteContent = {
+  "nav": {
+    "nav-item-1": "Services",
+    "nav-item-2": "Product",
+    "nav-item-3": "Vision",
+    "nav-item-4": "Features",
+    "nav-item-5": "About",
+    "nav-item-6": "Contact",
+    "img-src": "img/logo.png"
+  },
+  "cta": {
+    "h1": "DOM Is Awesome",
+    "button": "Get Started",
+    "img-src": "img/header-img.png"
+  },
+  "main-content": {
+    "features-h4":"Features",
+    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-h4":"About",
+    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "middle-img-src": "img/mid-page-accent.jpg",
+    "services-h4":"Services",
+    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-h4":"Product",
+    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-h4":"Vision",
+    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  },
+  "contact": {
+    "contact-h4" : "Contact",
+    "address" : "123 Way 456 Street Somewhere, USA",
+    "phone" : "1 (888) 888-8888",
+    "email" : "sales@greatidea.io",
+  },
+  "footer": {
+    "copyright" : "Copyright Great Idea! 2018"
+  },
+};
+
+// Example: Update the img src for the logo
+let logo = document.getElementById("logo-img");
+logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+
+
+
+let h = document.querySelector(".cta-text h1");
+h.textContent=siteContent['cta']['h1']
+
+
+let b = document.querySelector(".cta-text button");
+b.textContent = siteContent['cta']['button']
+
+let img = document.querySelector("#cta-img");
+img.src = siteContent['cta']['img-src']
+
+
+let f = document.querySelector(".top-content h4");
+f.textContent = siteContent['main-content']['features-h4']
+
+
+let f1 = document.querySelector(".text-content p")
+f1.textContent = siteContent['main-content']['features-content']
+
+
+
+let img1 = document.querySelector("#middle-img");
+img1.src = siteContent['main-content']['middle-img-src']
+
+let c = document.querySelector('.contact h4')
+c.textContent = siteContent['contact']['contact-h4']
+
+
+let c1 = document.querySelector('.contact p')
+c1.textContent = siteContent['contact']['address']
+
